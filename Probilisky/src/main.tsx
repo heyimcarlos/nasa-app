36ec159a5a67423952aea3509ad1121bedab1c62
SaaramRashidi <saaramnoman@gmail.com>
2025-10-05 11:19:03 -0400

    initial front end push
    initial push for frotend

---
+++ Probilisky/src/main.tsx
@@ -0,0 +1,14 @@
+// main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ThemeProvider } from "./components/theme-provider"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="system" enableSystem>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
